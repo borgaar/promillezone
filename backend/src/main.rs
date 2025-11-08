ad1ee528fdd3d7853b85f16e4e104156b744899f
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-11-08 17:42:10 +0100

    fixed docker

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -43,7 +43,7 @@ async fn main() {
         .layer(CorsLayer::permissive())
         .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Server listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
