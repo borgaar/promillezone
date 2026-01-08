38c8c70c348933bc06d5085b91cbca06856731c6
= <anders.morille@gmail.com>
2026-01-08 17:25:47 +0100

    Custom polling interval

--- frontend/lib/feature/time/widget/time_widget.dart
+++ frontend/lib/feature/time/widget/time_widget.dart
@@ -1,4 +1,3 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:promillezone/feature/kiosk/constants.dart';
 import 'package:promillezone/feature/kiosk/container.dart';
